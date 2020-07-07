266176ce9a2db905b28fdbfab6538d374721e3d8
seraphyne <renatafd717@gmail.com>
2020-07-07 11:56:56 -0500

    running sort and genre

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+/* eslint-disable indent */
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('common'));
 
-const playstore = require('./app-store');
+const appStore = require('./app-store.js');
+
+app.get('/apps', (req, res) => {
+      
+  const {sort, genre} = req.query;
+
+    if (sort) {
+        if(!['rating', 'app'].includes(sort)){
+        return res.send('Sort is invalid').status(400);
+    }}
+
+    if (genre) {
+        if(!['Action','Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(genre)) {
+        return res.send('Genre is invalid').status(400);
+    }}
+
+    let results = appStore.filter(app => 
+        app.Genres.toLowerCase().includes(genre.toLowerCase()));
+
+        if (sort) { results .sort((a, b) => { return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0; }); }
+
+        res.json(results);
+});
 
 
 
